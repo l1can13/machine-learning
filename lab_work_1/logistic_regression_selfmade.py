e33a3a5bab56f74572ab4567d252788fc584907c
l1can13 <a.a.soroka05@gmail.com>
2023-10-16 14:06:33 +0400

    selfmade NaiveBayesClassifier done (lab2)

--- lab_work_1/logistic_regression_selfmade.py
+++ lab_work_1/logistic_regression_selfmade.py
@@ -1,96 +1,105 @@
 import numpy as np
 
 
-class SelfmadeLogisticRegression:
-    def __init__(self, learning_rate=0.1, n_iterations=5000, reg_strength=1):
-        self.learning_rate = learning_rate
-        self.n_iterations = n_iterations
-        self.reg_strength = reg_strength
-        self.models_weights = {}
-        self.models_biases = {}
+class SelfmadeLogisticRegression:  # Определяем класс SelfmadeLogisticRegression.
+    def __init__(self, learning_rate=0.1, n_iterations=5000,
+                 reg_strength=1):  # Конструктор класса с параметрами по умолчанию.
+        self.learning_rate = learning_rate  # Устанавливаем скорость обучения.
+        self.n_iterations = n_iterations  # Устанавливаем количество итераций обучения.
+        self.reg_strength = reg_strength  # Устанавливаем коэффициент регуляризации.
+        self.models_weights = {}  # Инициализируем пустой словарь для хранения весов моделей.
+        self.models_biases = {}  # Инициализируем пустой словарь для хранения смещений моделей.
 
-    def sigmoid(self, z):
-        return 1 / (1 + np.exp(-z))
+    def sigmoid(self, z):  # Определяем метод для расчета сигмоидной функции.
+        return 1 / (1 + np.exp(-z))  # Возвращаем значение сигмоидной функции.
 
-    def softmax(self, z):
-        exp_z = np.exp(z - np.max(z, axis=1, keepdims=True))
-        return exp_z / np.sum(exp_z, axis=1, keepdims=True)
+    def softmax(self, z):  # Определяем метод для расчета softmax функции.
+        exp_z = np.exp(z - np.max(z, axis=1,
+                                  keepdims=True))  # Вычисляем экспоненты и вычитаем максимальное значение для стабильности.
+        return exp_z / np.sum(exp_z, axis=1, keepdims=True)  # Возвращаем нормализованные вероятности по классам.
 
-    def log_loss(self, y, predictions):
-        epsilon = 1e-15
-        predictions = np.clip(predictions, epsilon, 1 - epsilon)
-        loss = - np.sum(y * np.log(predictions))
-        return np.mean(loss)
+    def log_loss(self, y, predictions):  # Определяем метод для расчета функции потерь log loss.
+        epsilon = 1e-15  # Очень маленькое значение для стабильности.
+        predictions = np.clip(predictions, epsilon,
+                              1 - epsilon)  # Обрезаем предсказания для избежания выхода за границы логарифма.
+        loss = - np.sum(y * np.log(predictions))  # Рассчитываем log loss.
+        return np.mean(loss)  # Возвращаем среднее значение log loss по всем примерам.
 
-    def fit(self, X, y):
-        unique_classes = np.unique(y)
-        n_classes = len(unique_classes)
-        n_samples, n_features = X.shape
+    def fit(self, X, y):  # Определяем метод для обучения модели.
+        unique_classes = np.unique(y)  # Получаем уникальные классы в целевой переменной.
+        n_classes = len(unique_classes)  # Получаем количество классов.
+        n_samples, n_features = X.shape  # Получаем количество примеров и признаков.
 
-        for i, cls in enumerate(unique_classes):
-            binary_y = (y == cls).astype(int)
-            weights, bias = self.train_model(X, binary_y)
-            self.models_weights[i] = weights
-            self.models_biases[i] = bias
+        for i, cls in enumerate(unique_classes):  # Итерируемся по уникальным классам.
+            binary_y = (y == cls).astype(int)  # Преобразуем многоклассовую задачу в бинарную для текущего класса.
+            weights, bias = self.train_model(X, binary_y)  # Обучаем модель для бинарной классификации.
+            self.models_weights[i] = weights  # Сохраняем веса модели для текущего класса.
+            self.models_biases[i] = bias  # Сохраняем смещение модели для текущего класса.
 
-    def train_model(self, X, y):
-        n_samples, n_features = X.shape
-        weights = np.zeros(n_features)
-        bias = 0
+    def train_model(self, X, y):  # Определяем метод для обучения бинарной модели.
+        n_samples, n_features = X.shape  # Получаем количество примеров и признаков.
+        weights = np.zeros(n_features)  # Инициализируем веса нулями.
+        bias = 0  # Инициализируем смещение нулем.
 
-        for _ in range(self.n_iterations):
-            model = np.dot(X, weights) + bias
-            predictions = self.sigmoid(model)
+        for _ in range(self.n_iterations):  # Итерируемся по заданному числу итераций.
+            model = np.dot(X, weights) + bias  # Вычисляем модель.
+            predictions = self.sigmoid(model)  # Применяем сигмоиду к модели для получения предсказаний.
 
-            dw = (1 / n_samples) * (np.dot(X.T, (predictions - y)) + 2 * self.reg_strength * weights)
-            db = (1 / n_samples) * np.sum(predictions - y)
+            dw = (1 / n_samples) * (np.dot(X.T, (
+                    predictions - y)) + 2 * self.reg_strength * weights)  # Рассчитываем градиент по весам.
+            db = (1 / n_samples) * np.sum(predictions - y)  # Рассчитываем градиент по смещению.
 
-            weights -= self.learning_rate * dw
-            bias -= self.learning_rate * db
+            weights -= self.learning_rate * dw  # Обновляем веса.
+            bias -= self.learning_rate * db  # Обновляем смещение.
 
-        return weights, bias
+        return weights, bias  # Возвращаем обученные веса и смещение.
 
-    def predict(self, X):
-        predictions = np.zeros((X.shape[0], len(self.models_weights)))
+    def predict(self, X):  # Определяем метод для предсказания классов.
+        predictions = np.zeros((X.shape[0], len(self.models_weights)))  # Инициализируем массив для предсказаний.
 
-        for i, (weights, bias) in enumerate(zip(self.models_weights.values(), self.models_biases.values())):
-            model_predictions = np.dot(X, weights) + bias
-            predictions[:, i] = model_predictions
+        for i, (weights, bias) in enumerate(zip(self.models_weights.values(),
+                                                self.models_biases.values())):  # Итерируемся по сохраненным весам и смещениям.
+            model_predictions = np.dot(X, weights) + bias  # Вычисляем предсказания для текущей модели.
+            predictions[:, i] = model_predictions  # Сохраняем предсказания в массив.
 
-        return np.argmax(self.softmax(predictions), axis=1)
+        return np.argmax(self.softmax(predictions), axis=1)  # Возвращаем индексы классов с наибольшей вероятностью.
 
 
-def load_data(file_path):
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
+def load_data(file_path):  # Определяем функцию для загрузки данных.
+    with open(file_path, 'r') as file:  # Открываем файл для чтения.
+        lines = file.readlines()  # Считываем все строки из файла.
 
-    header = lines[0].strip().split(',')
-    data_lines = [line.strip().split(',') for line in lines[1:]]
+    header = lines[0].strip().split(',')  # Получаем заголовок данных.
+    data_lines = [line.strip().split(',') for line in lines[1:]]  # Преобразуем строки данных в список списков.
 
-    train_features = [list(map(float, line[:-2])) for line in data_lines if line[-1] == 'train']
-    train_labels = [int(line[-2]) for line in data_lines if line[-1] == 'train']
+    train_features = [list(map(float, line[:-2])) for line in data_lines if
+                      line[-1] == 'train']  # Извлекаем признаки для тренировочных данных.
+    train_labels = [int(line[-2]) for line in data_lines if
+                    line[-1] == 'train']  # Извлекаем метки классов для тренировочных данных.
 
-    test_features = [list(map(float, line[:-2])) for line in data_lines if line[-1] == 'test']
+    test_features = [list(map(float, line[:-2])) for line in data_lines if
+                     line[-1] == 'test']  # Извлекаем признаки для тестовых данных.
     # Добавляем лейбл -1 к тестовым данным
-    test_labels = [-1] * len(test_features)
+    test_labels = [-1] * len(test_features)  # Создаем список из -1 для меток тестовых данных.
 
     return (
-        np.array(train_features),
-        np.array(train_labels),
-        np.array(test_features),
-        np.array(test_labels)
+        np.array(train_features),  # Возвращаем массив тренировочных признаков.
+        np.array(train_labels),  # Возвращаем массив меток тренировочных данных.
+        np.array(test_features),  # Возвращаем массив тестовых признаков.
+        np.array(test_labels)  # Возвращаем массив меток тестовых данных.
     )
 
 
-def save_predictions(predictions, file_path):
-    np.savetxt(file_path, predictions, fmt='%d', delimiter=',')
+def save_predictions(predictions, file_path):  # Определяем функцию для сохранения предсказаний.
+    np.savetxt(file_path, predictions, fmt='%d',
+               delimiter=',')  # Сохраняем предсказания в файл с форматом '%d' и разделителем ','.
 
 
-x_train, y_train, x_test, tt_test = load_data('input.txt')
+x_train, y_train, x_test, tt_test = load_data('input.txt')  # Загружаем данные из файла 'input.txt'.
 
-model = SelfmadeLogisticRegression(n_iterations=5000)
-model.fit(x_train, y_train)
+model = SelfmadeLogisticRegression(n_iterations=5000)  # Создаем экземпляр модели с указанными параметрами.
+model.fit(x_train, y_train)  # Обучаем модель на тренировочных данных.
 
-selfmade_predictions = model.predict(x_test)
+selfmade_predictions = model.predict(x_test)  # Предсказываем классы для тестовых данных.
 
-save_predictions(selfmade_predictions, 'output.txt')
+save_predictions(selfmade_predictions, 'output.txt')  # Сохраняем предсказания в файл 'output.txt'.
---
+++ lab_work_2/naive_bayesian_qualifier.py
@@ -0,0 +1,140 @@
+import numpy as np  # Импорт библиотеки для работы с массивами и матрицами
+
+class SelfmadeNaiveBayesClassifier:
+    """
+    Наивный Байесовский классификатор.
+
+    Attributes:
+        num_labels (int): Количество уникальных меток.
+        label_data (list): Данные для каждой метки.
+        label_means (list): Средние значения для каждого признака для каждой метки.
+        label_stds (list): Стандартные отклонения для каждого признака для каждой метки.
+        label_priors (list): Априорные вероятности для каждой метки.
+    """
+
+    def __init__(self):
+        """
+        Инициализация объекта класса.
+        """
+        self.num_labels = 0  # Инициализация количества уникальных меток
+        self.label_data = []  # Инициализация списка данных для каждой метки
+        self.label_means = []  # Инициализация списка средних значений для каждого признака и метки
+        self.label_stds = []  # Инициализация списка стандартных отклонений для каждого признака и метки
+        self.label_priors = []  # Инициализация списка априорных вероятностей для каждой метки
+
+    def fit(self, training_features, training_labels):
+        """
+        Обучение классификатора на основе предоставленных данных.
+
+        Parameters:
+            training_features (numpy.ndarray): Массив тренировочных признаков.
+            training_labels (numpy.ndarray): Массив меток для тренировочных данных.
+
+        Returns:
+            None
+        """
+        self.num_labels = len(np.unique(training_labels))  # Определение количества уникальных меток
+        self.label_data = [training_features[training_labels == label] for label in range(self.num_labels)]  # Разделение данных для каждой метки
+        self.label_means = [np.nanmean(data, axis=0) if len(data) > 0 else np.zeros(training_features.shape[1]) for data in self.label_data]  # Расчет средних значений
+        self.label_stds = [np.nanstd(data, axis=0) if len(data) > 0 else np.ones(training_features.shape[1]) for data in self.label_data]  # Расчет стандартных отклонений
+        total_samples = len(training_labels)
+        self.label_priors = [len(data) / total_samples for data in self.label_data]  # Расчет априорных вероятностей
+
+    def predict(self, test_features):
+        """
+        Предсказание меток для тестовых данных.
+
+        Parameters:
+            test_features (numpy.ndarray): Массив тестовых признаков.
+
+        Returns:
+            list: Список предсказанных меток.
+        """
+        predictions = []  # Инициализация списка предсказанных меток
+        for test_instance in test_features:
+            probabilities = []  # Инициализация списка вероятностей
+            for label in range(self.num_labels):
+                prior_probability = self.label_priors[label]  # Получение априорной вероятности для метки
+                for feature_index in range(len(test_instance)):
+                    probability_density = self.calculate_probability(test_instance[feature_index],
+                                                                     self.label_means[label][feature_index],
+                                                                     self.label_stds[label][feature_index])
+                    prior_probability *= probability_density  # Обновление апостериорной вероятности
+                probabilities.append(prior_probability)
+            predicted_label = np.argmax(probabilities)  # Определение метки с наибольшей вероятностью
+            predictions.append(predicted_label)
+        return predictions
+
+    @staticmethod
+    def calculate_probability(value, mean, std):
+        """
+        Вычисление вероятности с использованием нормального распределения.
+
+        Parameters:
+            value (float): Значение признака.
+            mean (float): Среднее значение признака для метки.
+            std (float): Стандартное отклонение признака для метки.
+
+        Returns:
+            float: Рассчитанная вероятность.
+        """
+        if not std:
+            return 1
+        exponent = np.exp(-((value - mean) ** 2) / (2 * std ** 2))
+        return (1 / (std * np.sqrt(2 * np.pi))) * exponent
+
+
+def load_data(file_path):
+    """
+    Загрузка данных из файла.
+
+    Parameters:
+        file_path (str): Путь к файлу с данными.
+
+    Returns:
+        tuple: Кортеж с данными (train_features, train_labels, test_features, test_labels).
+    """
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    header = lines[0].strip().split(',')
+    data_lines = [line.strip().split(',') for line in lines[1:]]
+    train_features = [list(map(float, line[:-2])) for line in data_lines if line[-1] == 'train']
+    train_labels = [int(line[-2]) for line in data_lines if line[-1] == 'train']
+    test_features = [list(map(float, line[:-2])) for line in data_lines if line[-1] == 'test']
+    test_labels = [-1] * len(test_features)
+    return (
+        np.array(train_features),
+        np.array(train_labels),
+        np.array(test_features),
+        np.array(test_labels)
+    )
+
+
+def save_predictions(predictions, file_path):
+    """
+    Сохранение предсказаний в файл.
+
+    Parameters:
+        predictions (list): Список предсказанных меток.
+        file_path (str): Путь к файлу для сохранения.
+
+    Returns:
+        None
+    """
+    np.savetxt(file_path, predictions, fmt='%d', delimiter='\n')
+
+
+# Загрузка данных из входного файла
+train_features, train_labels, test_features, _ = load_data("input.txt")
+
+# Создание экземпляра классификатора
+classifier = SelfmadeNaiveBayesClassifier()
+
+# Обучение классификатора на тренировочных данных
+classifier.fit(train_features, train_labels)
+
+# Предсказание меток для тестовых данных
+predictions = classifier.predict(test_features)
+
+# Сохранение предсказаний в выходной файл
+save_predictions(predictions, "output.txt")
